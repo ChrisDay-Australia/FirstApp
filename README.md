93569f15a35b3af1d8be448f49c20c7ff0317e1d
ChrisDay-Australia <hk9YR7rngK5g3>
2015-05-01 00:53:58 +1000

    first message

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FirstApp
+
+Am creating the HTTP Server to start with to have somewhere to host the details
---
+++ appserver/node-express/lib/baseserver.js
@@ -0,0 +1,6 @@
+/**
+ * Created by chrisday on 1/05/15.
+ */
+var express = require('express');
+
+var app = express();
